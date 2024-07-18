d1a42582de65d613f5fe6ee0a63e4b496cb6fa23
Gaurav Rode <86872021+Gaurav98977@users.noreply.github.com>
2024-07-18 22:33:41 +0530

    Update Any_profit_percentage(Adding two or more rows ).sql

--- Any_profit_percentage(Adding two or more rows ).sql
+++ Any_profit_percentage(Adding two or more rows ).sql
@@ -1,32 +1,45 @@
 --- Calculating two or more rows by using ARRAY Function ----
-
-CREATE OR REPLACE FUNCTION calculate_ANYproduct_profit(product_id_input VARCHAR[])
+CREATE OR REPLACE FUNCTION calculate_ANYproduct_profit(product_ids_input VARCHAR[])
 RETURNS TABLE (
-    product_id VARCHAR,  
-    total_sales DOUBLE PRECISION,
-    total_profit DOUBLE PRECISION,
-    profit_percentage DOUBLE PRECISION
+    product_id VARCHAR,
+    total_sales NUMERIC,
+    total_profit NUMERIC,
+    profit_percentage NUMERIC
 ) AS $$
 BEGIN
     RETURN QUERY
     SELECT
         s.product_id AS product_id,
-        SUM(s.sales) AS total_sales,
-        SUM(s.profit) AS total_profit,
+        SUM(s.sales)::NUMERIC AS total_sales,
+        SUM(s.profit)::NUMERIC AS total_profit,
         CASE 
             WHEN SUM(s.sales) = 0 THEN 0
-            ELSE (SUM(s.profit) / SUM(s.sales)) * 100
+            ELSE ROUND((SUM(s.profit) / SUM(s.sales))::NUMERIC * 100, 2)  -- Round to 2 decimal places
         END AS profit_percentage
     FROM
         sales s
     WHERE  
-        s.product_id = ANY(product_id_input)
+        s.product_id = ANY(product_ids_input)
     GROUP BY
-        s.product_id;  -- for ensuring each product gets its own row
+        s.product_id;
 END;
 $$ LANGUAGE plpgsql;
 
 
+
+
 --Executing---
 SELECT product_id, total_sales, total_profit, profit_percentage
-FROM calculate_ANYproduct_profit (ARRAY['OFF-AR-10002833','FUR-CH-10002774']);
+FROM calculate_ANYproduct_profit (ARRAY['OFF-AR-10002833',
+'TEC-PH-10002275',
+'OFF-BI-10003910',
+'OFF-AP-10002892',
+'FUR-TA-10001539',
+'TEC-PH-10002033',
+'OFF-PA-10002365',
+'OFF-BI-10003656',
+'OFF-AP-10002311',
+'OFF-BI-10000756']);
+
+
+select * from sales
